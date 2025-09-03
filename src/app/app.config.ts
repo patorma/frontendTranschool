a05b5a7e9dac261fdc0edc0e6088dc593bc3e52d
patorma <pocontreras@ing.ucsc.cl>
2025-09-02 22:33:22 -0400

    se empezo la configuracion inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-
+//se configuran dependencias de terceros
+//se configuran servicios globales
+//se configuran rutas
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
